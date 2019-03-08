ddf52828adf01d7699aea7b2cccc34820971a2f3
josharmantrout91 <josh.armantrout@gmail.com>
2019-03-08 12:31:29 -0500

    Successful POST to login user and return welcome message and token

--- config/routes.js
+++ config/routes.js
@@ -6,6 +6,8 @@ const { authenticate } = require("../auth/authenticate");
 
 const Users = require("../users/users-model");
 
+const secrets = require("../secrets/secrets");
+
 module.exports = server => {
   server.post("/api/register", register);
   server.post("/api/login", login);
@@ -29,6 +31,35 @@ function register(req, res) {
 
 function login(req, res) {
   // implement user login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Welcome to the Fun House, ${user.username}`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+}
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+  const options = {
+    expiresIn: "1d"
+  };
+  return jwt.sign(payload, secrets.jwtSecret, options);
 }
 
 function getJokes(req, res) {
